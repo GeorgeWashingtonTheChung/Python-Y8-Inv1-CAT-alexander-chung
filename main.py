4399c7067547f543979bbcbf8ea5b0123fb29bd6
GeorgeWashingtonTheChung <alexanderkit.chung@student.education.wa.edu.au>
2026-04-22 09:43:15 +0800

    Dear Diary, Today I coded the recursive triangle of stars for task 4. It has an input area just in case.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Testing")
+def s_triangle(s):
+    # Prints a row of s no. of stars, then calls itself with s-1. Stops when s reaches 0
+    if s > 0:
+        print("*" * s)
+        s_triangle(s - 1)
+
+
+n = int(input("How many layers? "))
+s_triangle(n)
